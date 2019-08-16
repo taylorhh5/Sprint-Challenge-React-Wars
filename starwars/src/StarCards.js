0147e4366212d967762b8b238d188c8558764ddc
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-08-16 13:54:21 -0400

    added styling

--- starwars/src/StarCards.js
+++ starwars/src/StarCards.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Card} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
+import { Image } from 'semantic-ui-react'
 
 
 const StarCard = props => {
@@ -27,6 +28,7 @@ const StarCard = props => {
   header="This person's gender is:"
   
  description={props.gender} />
+
 
  </div>
   );
